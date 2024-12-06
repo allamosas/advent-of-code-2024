48542db1cfa1a527b1ed065d60ff37b93d159b16
allamobar <allamobar@gmail.com>
2024-12-06 05:36:38 +0100

    Día de soporte tecnico

--- 03/03_part1.py
+++ 03/03_part1.py
@@ -24,7 +24,6 @@ regex_split = r'\d{1,3},\d{1,3}'
 #Se recorren todas las coincidencias y se pasan a una lista de enteros
 numbers = []
 for mul in muls:
-    new_row = []    #Se crea una nueva fila y se añaden los dos numeros parseados a int
     pair = re.findall(regex_split, mul)                        # "abc,xyz"       
     numbers.append([int(num) for num in pair[0].split(",")])   # [abc][xyz] #Se añade a numbers
 
